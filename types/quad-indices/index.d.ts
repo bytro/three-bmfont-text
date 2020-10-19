0e4341425c08a73bb1c4b75728fb4219ed54b875
Oliver Paschke <oliver.paschke@bytro.com>
2020-10-19 14:39:06 +0200

    Move typing into separate repository

--- types/quad-indices/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'quad-indices' {
-    interface QuadOptions {
-        clockwise: boolean;
-        type: string;
-        count: number;
-    }
-    function createQuadElements(opt: QuadOptions): number[];
-    export = createQuadElements;
-}
